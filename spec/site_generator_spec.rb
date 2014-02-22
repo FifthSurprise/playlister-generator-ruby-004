8ecc00b025d69aaff9b4a08a39eac0fe97fab0eb
Kevin Chang <kevin.w.chang@gmail.com>
2014-02-22 16:30:37 -0500

    added rspec testing for site generator

--- spec/site_generator_spec.rb
+++ spec/site_generator_spec.rb
@@ -1,81 +1,63 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe 'SiteGenerator' do
-#   let(:site_generator) { SiteGenerator.new }
-#   let(:file_name) {[
-#     "the_matrix.html",
-#     "the_lego_movie.html",
-#     "robocop.html",
-#     "the_wolf_of_wall_street.html",
-#     "frozen.html",
-#     "logan's_run.html",
-#     "a_fish_called_wanda.html",
-#     "the_godfather.html",
-#     "apocalypse_now.html",
-#     "the_sting.html",
-#     "temple_grandin.html",
-#     "gravity.html",
-#     "empire_of_the_sun.html",
-#     "the_big_lebowski.html",
-#     "pan's_labyrinth.html",
-#     "unbreakable.html",
-#     "gattaca.html",
-#     "the_dark_knight.html",
-#     "donnie_darko.html",
-#     "rescue_dawn.html",
-#     "the_prestige.html",
-#     "memento.html",
-#     "avatar.html",
-#     "killing_them_softly.html",
-#     "300.html"
-#   ]}
+describe 'SiteGenerator' do
+  let(:site_generator) { SiteGenerator.new }
 
-#   before do
-#     lp = LibraryParser.new(DATA_PATH)
-#     Artist.reset_all!
-#   end
+  before do
+    Artist.reset_all
+    Genre.reset_all
+    Song.reset_all
+    lp = LibraryParser.new(DATA_PATH)
+    lp.parse
+  end
 
-#   after do
-#     File.open('_site/index.html', 'w') do |f|
-#       f.write('')
-#       f.close
-#     end
-#   end
+  after do
+    File.open('_site/genres.html', 'w') do |f|
+      f.write('')
+      f.close
+    end
+    File.open('_site/artists.html', 'w') do |f|
+      f.write('')
+      f.close
+    end
+  end
 
-#   describe '#make_index!' do
-#     it 'creates index.html in the _site directory' do
-#       site_generator.make_index!
-#       comparison = File.read('spec/fixtures/index.html')
-#       index = File.read('_site/index.html')
-#       expect(index).to eq(comparison)
-#     end
+  describe '#make_artist_index!' do
+    it 'creates artists.html in the _site directory' do
+      site_generator.make_artist_index!
+      comparison = File.read('spec/fixtures/artists.html')
+      index = File.read('_site/artists.html')
+      expect(index).to eq(comparison)
+    end
+  end
 
-#     it 'does NOT use erb' do
-#       expect(ERB).to_not receive(:new)
-#       site_generator.make_index!
-#     end
-#   end
+  describe '#make_genre_index!' do
+    it 'creates genres.html in the _site directory' do
+      site_generator.make_genre_index!
+      comparison = File.read('spec/fixtures/genres.html')
+      index = File.read('_site/genres.html')
+      expect(index).to eq(comparison)
+    end
+  end
 
-#   describe '#generate_pages!' do
-#     after do
-#       FileUtils.rm(Dir.glob('_site/movies/*.html'))
-#     end
+  describe '#generate_pages!' do
+    after do
+      FileUtils.rm(Dir.glob('_site/genre/*.html'))
+      FileUtils.rm(Dir.glob('_site/artist/*.html'))
+      FileUtils.rm(Dir.glob('_site/song/*.html'))
+    end
 
-#     it 'creates an html page for each movie in the _site/movies directory' do
-#       site_generator.generate_pages!
-#       expect(Dir.entries('_site/movies').reject{|e| e.start_with?('.')}.size).to eq(25)
-#     end
-
-#     it 'makes html pages that follow a specific layout' do
-#       site_generator.generate_pages!
-#       the_matrix = File.read('_site/movies/the_matrix.html').gsub("\n",'').gsub(' ','')
-#       comparison = File.read('spec/fixtures/the_matrix.html').gsub("\n",'').gsub(' ','')
-#       expect(the_matrix).to eq(comparison)
-#     end
-
-#     it 'uses ERB' do
-#       expect_any_instance_of(ERB).to receive(:result).at_least(25).times
-#       site_generator.generate_pages!
-#     end
-#   end
-# end
+    it 'creates an html page for each genre in the _site/genre directory' do
+      site_generator.generate_Genre_Pages
+      expect(Dir.entries('_site/genre').reject{|e| e.start_with?('.')}.size).to eq(10)
+    end
+    it 'creates an html page for each song in the _site/song directory' do
+      site_generator.generate_Song_Pages
+      expect(Dir.entries('_site/song').reject{|e| e.start_with?('.')}.size).to eq(99)
+    end
+    it 'creates an html page for each artist in the _site/artist directory' do
+      site_generator.generate_Artist_Pages
+      expect(Dir.entries('_site/artist').reject{|e| e.start_with?('.')}.size).to eq(90)
+    end
+  end
+end
